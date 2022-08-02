5299d5291c3c63fa7b4a3abc1107483bcf9c7ecb
Irene Njeru <irenenjeru8@gmail.com>
2022-08-02 19:12:38 +0300

    task completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :recipes, only: [:index, :create]
+  
+  # get "/recipes", to: "recipes#indexx" 
+  delete "/logout", to: "sessions#destroy"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
